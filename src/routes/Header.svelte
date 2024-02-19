8a62f6a46143db2a6657aa75fb85ec26043d4765
Adrien M <adrien.mussat@epita.fr>
2024-02-19 19:05:04 +0100

    Fixing path bug

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,27 +1,24 @@
 <script>
-	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+    import { base } from '$app/paths';
 </script>
 
 <header class="bg-gray-800 text-gray-100">
-	<nav class="flex items-center justify-center p-5">
-		<div class="flex items-center space-x-4">
-		<ul class="flex justify-center space-x-10">
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About Me</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/') ? 'page' : undefined}>
-				<a href="/projects"> My projects</a>
-			</li>
-			<li>
-				<a href="https://github.com/AdiLeTwix">GitHub</a>
-			</li>
-		</ul>
-	</div>
-	</nav>
-
+    <nav class="flex items-center justify-center p-5">
+        <div class="flex items-center space-x-4">
+        <ul class="flex justify-center space-x-10">
+            <li>
+                <a href="{`${base}/`}" rel="prefetch">Home</a>
+            </li>
+            <li>
+                <a href="{`${base}/about`}" rel="prefetch">About Me</a>
+            </li>
+            <li>
+                <a href="{`${base}/projects`}" rel="prefetch">My projects</a>
+            </li>
+            <li>
+                <a href="https://github.com/AdiLeTwix">GitHub</a>
+            </li>
+        </ul>
+    </div>
+    </nav>
 </header>
